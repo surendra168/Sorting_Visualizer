778b6d0edabcbb88828f535792f3df6cfe66dba8
surendra <reddysurendra294@gmail.com>
2024-05-17 18:01:49 +0530

    changes to dropdown

--- src/assets/DropDown/Dropdown.js
+++ src/assets/DropDown/Dropdown.js
@@ -1,19 +1,17 @@
-import React from 'react'
-import classes from './dropdown.module.css'
+import React from "react";
+import classes from "./dropdown.module.css";
 
 const Dropdown = ({ onChange, disabled }) => {
   return (
     <div className={classes.dropdown}>
       <select onChange={onChange} disabled={disabled}>
-        <option value='bubbleSort'>Bubble Sort</option>
-        <option value='selectionSort'>Selection Sort</option>
-        <option value='insertionSort'>Insertion Sort</option>
-        <option value='mergeSort'>Merge Sort</option>
-        <option value='quickSort'>Quick Sort</option>
-        <option value='heapSort'>Heap Sort</option>
+        <option value="bubbleSort">Bubble Sort</option>
+        <option value="mergeSort">Merge Sort</option>
+        <option value="quickSort">Quick Sort</option>
+        <option value="heapSort">Heap Sort</option>
       </select>
     </div>
-  )
-}
+  );
+};
 
-export default Dropdown
+export default Dropdown;
